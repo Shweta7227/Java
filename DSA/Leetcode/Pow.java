475042519c5d4a5ebfcc8cedd25416c794557318
Shweta7227 <sindhushweta02@gmail.com>
2026-01-07 21:40:41 +0530

    solution is added

---
+++ DSA/Leetcode/Pow.java
@@ -0,0 +1,51 @@
+import javax.swing.tree.TreeNode;
+
+public class Pow {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+    private static final int MOD = 1_000_000_007;
+    private long totalSum = 0;
+    private long maxProduct = 0;
+
+    public int maxProduct(TreeNode root) {
+        totalSum = treeSum(root);
+        computeSubtree(root);
+        return (int) (maxProduct % MOD);
+    }
+
+    // First DFS: total sum
+    private long treeSum(TreeNode node) {
+        if (node == null) return 0;
+        return node.val + treeSum(node.left) + treeSum(node.right);
+    }
+
+    // Second DFS: subtree sums + product calculation
+    private long computeSubtree(TreeNode node) {
+        if (node == null) return 0;
+
+        long left = computeSubtree(node.left);
+        long right = computeSubtree(node.right);
+
+        long subtreeSum = node.val + left + right;
+
+        long product = subtreeSum * (totalSum - subtreeSum);
+        maxProduct = Math.max(maxProduct, product);
+
+        return subtreeSum;
+    }
+}
+
